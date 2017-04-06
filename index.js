5a4b3c73c01ac70ea5ea7e2917c75a068d0c80dd
Phil Myers <prm5031@gmail.com>
2017-04-06 10:38:03 -0400

    Create NBAClient; implement GetTankRankings intent

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
-
+const compoundSubject = require('compound-subject');
 require('dotenv').config();
+
 const Alexa = require('alexa-sdk');
+const NBAClient = require('./src/NBAClient');
+const teams = require('./src/teams');
 
 const APP_ID = process.env.APP_ID;
+const NUM_TEAMS = process.env.NUM_TEAMS;
 
 const strings = {
   'HELP_MESSAGE': 'NBA Tank Rankings can tell you the latest standings for the NBA draft lottery. Try asking, ' +
@@ -19,6 +23,17 @@ exports.handler = function (event, context, callback) {
 };
 
 const handlers = {
+  'GetTankRankings': function () {
+    const nbaClient = new NBAClient();
+    console.log('Getting tank rankings');
+    nbaClient.getStandingsJSON(standingsResponse => {
+      if (standingsResponse.statusCode >= 200 && standingsResponse.statusCode <= 399) {
+        const topThree = standingsResponse.standingsJSON.slice(0, NUM_TEAMS).map(team => 'the ' + teams.league.standard[team.teamId].nickname);
+        this.emit(':tell', 'The top ' + NUM_TEAMS + ' teams in the tank rankings are ' + compoundSubject(topThree).delimitAll().make());
+      }
+    });
+  },
+
   'AMAZON.HelpIntent': function () {
     this.emit(':ask', strings.HELP_MESSAGE, strings.HELP_MESSAGE);
   },
---
+++ src/NBAClient.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const _ = require('lodash');
+
+/**
+Inspired by https://github.com/alexa/skill-sample-node-device-address-api/blob/master/src/AlexaDeviceAddressClient.js
+**/
+class NBAClient {
+  constructor () {
+    this.endpoint = 'data.nba.net';
+  }
+
+  getStandingsJSON (callback) {
+    let standingsRequest = this._getStandingsRequest();
+
+    standingsRequest.then(callback);
+  }
+
+  _getStandingsRequest () {
+    const options = this._getRequestOptions('/data/10s/prod/v1/current/standings_conference.json');
+
+    return new Promise((resolve, reject) => {
+      this._handleRequest(options, resolve, reject);
+    });
+  }
+
+  _handleRequest (options, resolve, reject) {
+    http.get(options, response => {
+      let rawData = '';
+      response.on('data', (chunk) => {
+        rawData += chunk;
+      });
+      response.on('end', () => {
+        const standingsJSON = JSON.parse(rawData);
+        const eastStandings = _.get(standingsJSON, 'league.standard.conference.east', []);
+        const westStandings = _.get(standingsJSON, 'league.standard.conference.west', []);
+        let leagueStandings = eastStandings.concat(westStandings);
+        leagueStandings = _.orderBy(leagueStandings, team => team.sortKey.winPct, ['desc']);
+        const nbaResponse = {
+          statusCode: response.statusCode,
+          standingsJSON: leagueStandings
+        };
+
+        resolve(nbaResponse);
+      }).on('error', e => {
+        console.error(e);
+        reject();
+      });
+    });
+  }
+
+  _getRequestOptions (path) {
+    return {
+      hostname: this.endpoint,
+      path: path,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) ' +
+        'Chrome/56.0.2924.87 Safari/537.36',
+        'Accept': 'application/json'
+      }
+    };
+  }
+}
+
+module.exports = NBAClient;
