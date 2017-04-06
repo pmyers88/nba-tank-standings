e225b7ff41dee916056ed0ad637840780ff01f17
Phil Myers <prm5031@gmail.com>
2017-04-06 16:01:05 -0400

    Setup testing; write NBAClient unit test; fix problems exposed by unit test

--- src/NBAClient.js
+++ src/NBAClient.js
@@ -4,20 +4,24 @@ const _ = require('lodash');
 // Inspired by https://github.com/alexa/skill-sample-node-device-address-api/blob/master/src/AlexaDeviceAddressClient.js
 class NBAClient {
   constructor () {
-    this.endpoint = 'data.nba.net';
+    this._endpoint = 'data.nba.net';
   }
 
-  getStandingsJSON (callback) {
-    let standingsRequest = this._getStandingsRequest();
-
-    standingsRequest.then(callback);
-  }
-
-  _getStandingsRequest () {
+  getStandingsRequest () {
     const options = this._getRequestOptions('/data/10s/prod/v1/current/standings_conference.json');
 
     return new Promise((resolve, reject) => {
-      this._handleRequest(options, resolve, reject);
+      this._handleRequest(options, (nbaResponse) => {
+        if (nbaResponse.statusCode >= 200 && nbaResponse.statusCode <= 399) {
+          const eastStandings = _.get(nbaResponse.json, 'league.standard.conference.east', []);
+          const westStandings = _.get(nbaResponse.json, 'league.standard.conference.west', []);
+          let leagueStandings = eastStandings.concat(westStandings);
+          leagueStandings = _.orderBy(leagueStandings, team => team.sortKey.winPct, ['desc']);
+          resolve(leagueStandings);
+        } else {
+          reject(new Error(`Received invalid HTTP response code: ${nbaResponse.statusCode}`));
+        }
+      }, reject);
     });
   }
 
@@ -28,27 +32,27 @@ class NBAClient {
         rawData += chunk;
       });
       response.on('end', () => {
-        const standingsJSON = JSON.parse(rawData);
-        const eastStandings = _.get(standingsJSON, 'league.standard.conference.east', []);
-        const westStandings = _.get(standingsJSON, 'league.standard.conference.west', []);
-        let leagueStandings = eastStandings.concat(westStandings);
-        leagueStandings = _.orderBy(leagueStandings, team => team.sortKey.winPct, ['desc']);
-        const nbaResponse = {
-          statusCode: response.statusCode,
-          standingsJSON: leagueStandings
-        };
+        let responseJSON;
+        try {
+          responseJSON = {
+            statusCode: response.statusCode,
+            json: JSON.parse(rawData)
+          };
+        } catch (e) {
+          console.error(e.message);
+        }
 
-        resolve(nbaResponse);
-      }).on('error', e => {
-        console.error(e);
-        reject();
+        resolve(responseJSON);
       });
+    }).on('error', e => {
+      console.error(e.message);
+      reject(e);
     });
   }
 
   _getRequestOptions (path) {
     return {
-      hostname: this.endpoint,
+      hostname: this._endpoint,
       path: path,
       headers: {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) ' +
---
+++ test/unit/NBAClientTest.js
@@ -0,0 +1,142 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const PassThrough = require('stream').PassThrough;
+const http = require('http');
+
+const NBAClient = require('../../src/NBAClient');
+const standings = require('../fixtures/standings_conference');
+
+chai.should();
+
+describe('NBAClient', function () {
+  let nbaClient;
+  const endpoint = 'data.nba.net';
+  const standingsPath = '/data/10s/prod/v1/current/standings_conference.json';
+
+  beforeEach(function () {
+    nbaClient = new NBAClient();
+  });
+
+  describe('#constructor', function () {
+    it('should create an NBAClient with the correct defaults', function () {
+      const client = new NBAClient();
+      client._endpoint.should.equal(endpoint);
+    });
+  });
+
+  describe('#getStandingsRequest', function () {
+    let handleRequest;
+
+    beforeEach(function () {
+      handleRequest = sinon.stub(nbaClient, '_handleRequest');
+    });
+
+    afterEach(function () {
+      nbaClient._handleRequest.restore();
+    });
+
+    it('should return NBA teams sorted by winning percentage when promise resolves', function () {
+      const expected = {statusCode: 200, json: standings};
+      handleRequest.callsArgWith(1, expected);
+
+      const standingsRequest = nbaClient.getStandingsRequest();
+      return standingsRequest.then(nbaStandings => {
+        nbaStandings.length.should.equal(30);
+        let winPct = '.000';
+        nbaStandings.forEach(team => {
+          team.should.include.keys('teamId');
+          winPct.should.be.at.most(team.winPct);
+          winPct = team.winPct;
+        });
+      });
+    });
+
+    it('should reject when status code is in the 400 range', function () {
+      const expected = {statusCode: 400, json: {}};
+      handleRequest.callsArgWith(1, expected);
+
+      const standingsRequest = nbaClient.getStandingsRequest();
+      return standingsRequest.catch(error => {
+        error.message.should.equal(`Received invalid HTTP response code: 400`);
+      });
+    });
+
+    it('should reject the promise on request error', function () {
+      const message = 'there was an error with your request';
+      const expected = new Error('there was an error with your request');
+
+      handleRequest.callsArgWith(2, expected);
+
+      const standingsRequest = nbaClient.getStandingsRequest();
+
+      return standingsRequest.catch(error => {
+        error.message.should.equal(message);
+      });
+    });
+  });
+
+  describe('#_handleRequest', function () {
+    let get;
+
+    beforeEach(function () {
+      get = sinon.stub(http, 'get');
+    });
+
+    afterEach(function () {
+      http.get.restore();
+    });
+
+    it('should resolve the promise with an nbaStandingsResponse on end', function (done) {
+      const expected = {foo: 'bar', baz: 'foobar', num: 1};
+      const response = new PassThrough();
+      response.statusCode = 200;
+      response.write(JSON.stringify(expected));
+      response.end();
+
+      const request = new PassThrough();
+      get.callsArgWith(1, response).returns(request);
+
+      nbaClient._handleRequest({hostname: endpoint, path: standingsPath}, result => {
+        result.statusCode.should.equal(200);
+        result.json.should.eql(expected);
+        done();
+      });
+    });
+
+    it('should reject the promise on error', function (done) {
+      const expected = 'there was an error with your request';
+      const request = new PassThrough();
+
+      get.returns(request);
+
+      nbaClient._handleRequest({}, () => {}, error => {
+        error.should.equal(expected);
+        done();
+      });
+
+      request.emit('error', expected);
+    });
+  });
+
+  describe('#_getRequestOptions', function () {
+    it('should return the correct request options', function () {
+      const headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) ' +
+        'Chrome/56.0.2924.87 Safari/537.36',
+        'Accept': 'application/json'
+      };
+      let options = nbaClient._getRequestOptions(standingsPath);
+      options.should.eql({
+        hostname: endpoint,
+        path: '/data/10s/prod/v1/current/standings_conference.json',
+        headers: headers
+      });
+      options = nbaClient._getRequestOptions('/foo.json');
+      options.should.eql({
+        hostname: endpoint,
+        path: '/foo.json',
+        headers: headers
+      });
+    });
+  });
+});
