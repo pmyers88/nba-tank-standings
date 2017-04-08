668c43828c1d09a47b656885a9017111c3bd1b5c
Phil Myers <prm5031@gmail.com>
2017-04-08 02:05:54 -0400

    Split out events and intents into their own files

---
+++ src/events.js
@@ -0,0 +1,8 @@
+const events = {
+  NEW_SESSION: 'NewSession',
+  LAUNCH_REQUEST: 'LaunchRequest',
+  SESSION_ENDED: 'SessionEndedRequest',
+  UNHANDLED: 'Unhandled'
+};
+
+module.exports = events;
---
+++ src/intents.js
@@ -0,0 +1,11 @@
+const intents = {
+  GET_TANK_RANKINGS: 'GetTankRankings',
+  GET_LOTTERY_SIMULATION: 'GetLotterySimulation',
+  GET_TOP_N_TANK_RANKINGS: 'GetTopNTankRankings',
+  GET_TEAM_RANKINGS: 'GetTeamRankings',
+  AMAZON_HELP: 'AMAZON.HelpIntent',
+  AMAZON_STOP: 'AMAZON.StopIntent',
+  AMAZON_CANCEL: 'AMAZON.CancelIntent'
+};
+
+module.exports = intents;
