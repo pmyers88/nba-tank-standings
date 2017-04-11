2bc04d5cba11f48b9b5ea60c8446770a17bc3a20
Phil Myers <prm5031@gmail.com>
2017-04-11 15:30:33 -0400

    Create function to return winner for each pick and test it

--- src/lotteryOdds.js
+++ src/lotteryOdds.js
@@ -1,7 +1,22 @@
-const lotteryOdds = {
+const _ = require('lodash');
+
+const _lotteryOdds = {
   1: [250, 199, 156, 119, 88, 63, 43, 28, 17, 11, 8, 7, 6, 5],
   2: [215, 188, 157, 126, 97, 71, 49, 33, 20, 13, 9, 8, 7, 6],
   3: [178, 171, 156, 133, 107, 81, 58, 39, 24, 16, 12, 10, 9, 7]
 };
 
-module.exports = lotteryOdds;
+const selectWinnerForPick = (pickNum) => {
+  const weightedArray = _lotteryOdds[pickNum];
+  const total = weightedArray.reduce((acc, num) => acc + num, 0);
+  const randomNum = _.random(1, total);
+  let sum = 0;
+  return _.findIndex(weightedArray, (val) => {
+    sum += val;
+    return randomNum <= sum;
+  });
+};
+
+module.exports = {
+  selectWinnerForPick: selectWinnerForPick
+};
--- test/unit/lotteryOddsTest.js
+++ test/unit/lotteryOddsTest.js
@@ -1,22 +1,169 @@
 const chai = require('chai');
-const lotteryOdds = require('../../src/lotteryOdds');
+const sinon = require('sinon');
+const rewire = require('rewire');
+const _ = require('lodash');
 
-describe('lotteryOdds', function () {
-  it('should only have keys 1, 2, 3', function () {
-    lotteryOdds.should.have.keys('1', '2', '3');
+const lotteryOdds = rewire('../../src/lotteryOdds');
+
+describe('LotteryOdds', function () {
+  describe('#_lotteryOdds', function () {
+    const _lotteryOdds = lotteryOdds.__get__('_lotteryOdds');
+
+    it('should only have keys 1, 2, 3', function () {
+      _lotteryOdds.should.have.keys('1', '2', '3');
+    });
+
+    it('the sum of the values for the first pick should be 1000', function () {
+      _lotteryOdds['1'].reduce((acc, val) => acc + val, 0).should.equal(1000);
+    });
+
+    it('the sum of the values for the second pick should be 999', function () {
+      _lotteryOdds['2'].reduce((acc, val) => acc + val, 0).should.equal(999);
+    });
+
+    it('the sum of the values for the third pick should be 1001', function () {
+      _lotteryOdds['3'].reduce((acc, val) => acc + val, 0).should.equal(1001);
+    });
   });
 
-  it('the sum of the values for the first pick should be 1000', function () {
-    lotteryOdds['1'].reduce((acc, val) => acc + val, 0).should.equal(1000);
-  });
+  describe('#selectWinnerForPick', function () {
+    describe('with pickNum === 1', function () {
+      let random;
 
-  it('the sum of the values for the second pick should be 999', function () {
-    lotteryOdds['2'].reduce((acc, val) => acc + val, 0).should.equal(999);
-  });
+      beforeEach(function () {
+        random = sinon.stub(_, 'random');
+      });
 
-  it('the sum of the values for the third pick should be 1001', function () {
-    lotteryOdds['3'].reduce((acc, val) => acc + val, 0).should.equal(1001);
+      afterEach(function () {
+        _.random.restore();
+      });
+
+      it('should return 0 for 1 <= randomNum <= 250', function () {
+        random.returns(1);
+        lotteryOdds.selectWinnerForPick(1).should.equal(0);
+        random.returns(51);
+        lotteryOdds.selectWinnerForPick(1).should.equal(0);
+        random.returns(250);
+        lotteryOdds.selectWinnerForPick(1).should.equal(0);
+      });
+
+      it('should return 1 for 251 <= randomNum <= 449', function () {
+        random.returns(251);
+        lotteryOdds.selectWinnerForPick(1).should.equal(1);
+        random.returns(300);
+        lotteryOdds.selectWinnerForPick(1).should.equal(1);
+        random.returns(449);
+        lotteryOdds.selectWinnerForPick(1).should.equal(1);
+      });
+
+      it('should return 2 for 450 <= randomNum <= 605', function () {
+        random.returns(450);
+        lotteryOdds.selectWinnerForPick(1).should.equal(2);
+        random.returns(500);
+        lotteryOdds.selectWinnerForPick(1).should.equal(2);
+        random.returns(605);
+        lotteryOdds.selectWinnerForPick(1).should.equal(2);
+      });
+
+      it('should return 3 for 606 <= randomNum <= 724', function () {
+        random.returns(606);
+        lotteryOdds.selectWinnerForPick(1).should.equal(3);
+        random.returns(700);
+        lotteryOdds.selectWinnerForPick(1).should.equal(3);
+        random.returns(724);
+        lotteryOdds.selectWinnerForPick(1).should.equal(3);
+      });
+
+      it('should return 4 for 725 <= randomNum <= 812', function () {
+        random.returns(725);
+        lotteryOdds.selectWinnerForPick(1).should.equal(4);
+        random.returns(800);
+        lotteryOdds.selectWinnerForPick(1).should.equal(4);
+        random.returns(812);
+        lotteryOdds.selectWinnerForPick(1).should.equal(4);
+      });
+
+      it('should return 5 for 813 <= randomNum <= 875', function () {
+        random.returns(813);
+        lotteryOdds.selectWinnerForPick(1).should.equal(5);
+        random.returns(850);
+        lotteryOdds.selectWinnerForPick(1).should.equal(5);
+        random.returns(875);
+        lotteryOdds.selectWinnerForPick(1).should.equal(5);
+      });
+
+      it('should return 6 for 876 <= randomNum <= 918', function () {
+        random.returns(876);
+        lotteryOdds.selectWinnerForPick(1).should.equal(6);
+        random.returns(900);
+        lotteryOdds.selectWinnerForPick(1).should.equal(6);
+        random.returns(918);
+        lotteryOdds.selectWinnerForPick(1).should.equal(6);
+      });
+
+      it('should return 7 for 919 <= randomNum <= 946', function () {
+        random.returns(919);
+        lotteryOdds.selectWinnerForPick(1).should.equal(7);
+        random.returns(930);
+        lotteryOdds.selectWinnerForPick(1).should.equal(7);
+        random.returns(946);
+        lotteryOdds.selectWinnerForPick(1).should.equal(7);
+      });
+
+      it('should return 8 for 947 <= randomNum <= 963', function () {
+        random.returns(947);
+        lotteryOdds.selectWinnerForPick(1).should.equal(8);
+        random.returns(955);
+        lotteryOdds.selectWinnerForPick(1).should.equal(8);
+        random.returns(963);
+        lotteryOdds.selectWinnerForPick(1).should.equal(8);
+      });
+
+      it('should return 9 for 964 <= randomNum <= 974', function () {
+        random.returns(964);
+        lotteryOdds.selectWinnerForPick(1).should.equal(9);
+        random.returns(970);
+        lotteryOdds.selectWinnerForPick(1).should.equal(9);
+        random.returns(974);
+        lotteryOdds.selectWinnerForPick(1).should.equal(9);
+      });
+
+      it('should return 10 for 975 <= randomNum <= 982', function () {
+        random.returns(975);
+        lotteryOdds.selectWinnerForPick(1).should.equal(10);
+        random.returns(980);
+        lotteryOdds.selectWinnerForPick(1).should.equal(10);
+        random.returns(982);
+        lotteryOdds.selectWinnerForPick(1).should.equal(10);
+      });
+
+      it('should return 11 for 983 <= randomNum <= 989', function () {
+        random.returns(983);
+        lotteryOdds.selectWinnerForPick(1).should.equal(11);
+        random.returns(986);
+        lotteryOdds.selectWinnerForPick(1).should.equal(11);
+        random.returns(989);
+        lotteryOdds.selectWinnerForPick(1).should.equal(11);
+      });
+
+      it('should return 12 for 990 <= randomNum <= 995', function () {
+        random.returns(990);
+        lotteryOdds.selectWinnerForPick(1).should.equal(12);
+        random.returns(992);
+        lotteryOdds.selectWinnerForPick(1).should.equal(12);
+        random.returns(995);
+        lotteryOdds.selectWinnerForPick(1).should.equal(12);
+      });
+
+      it('should return 13 for 996 <= randomNum <= 1000', function () {
+        random.returns(996);
+        lotteryOdds.selectWinnerForPick(1).should.equal(13);
+        random.returns(997);
+        lotteryOdds.selectWinnerForPick(1).should.equal(13);
+        random.returns(1000);
+        lotteryOdds.selectWinnerForPick(1).should.equal(13);
+      });
+    });
   });
 });
-
 chai.should();
