c76a4240d4e7aa1082740c0d6318d60c441e664f
Phil Myers <prm5031@gmail.com>
2017-04-20 23:04:23 -0400

    Add comments to explain lottery drawing algorithm

--- src/lotteryOdds.js
+++ src/lotteryOdds.js
@@ -2,6 +2,7 @@
 
 const _ = require('lodash');
 
+// source for odds: https://en.wikipedia.org/wiki/NBA_draft_lottery
 const _lotteryOdds = {
   1: [250, 199, 156, 119, 88, 63, 43, 28, 17, 11, 8, 7, 6, 5],
   2: [215, 188, 157, 126, 97, 71, 49, 33, 20, 13, 9, 8, 7, 6],
@@ -10,17 +11,20 @@ const _lotteryOdds = {
 
 const selectWinnerForPick = (pickNum, alreadySelected) => {
   const weightedArray = _lotteryOdds[pickNum];
-  const total = weightedArray.reduce((acc, num) => acc + num, 0);
-  let index;
+  const sumChancesForPick = weightedArray.reduce((acc, num) => acc + num, 0);
+  let winnerIndex;
   do {
-    const randomNum = _.random(1, total);
+    const randomNum = _.random(1, sumChancesForPick);
     let sum = 0;
-    index = _.findIndex(weightedArray, (val) => {
+    // team 0 has 250 chances, so random drawing between 1 and 250 == team0
+    // team 1 has 199 chances, so random drawing between 251 and 459 == team1
+    // etc.
+    winnerIndex = _.findIndex(weightedArray, (val) => {
       sum += val;
       return randomNum <= sum;
     });
-  } while (_.findIndex(alreadySelected, num => index === num) !== -1);
-  return index;
+  } while (_.findIndex(alreadySelected, num => winnerIndex === num) !== -1);
+  return winnerIndex;
 };
 
 module.exports = {
